dad4b6822793040d0e1f477b3c38d7212a815c19
Henrik Söderlind <henriksoderlind@MacBook-Pro-som-tillhor-Henrik-2.local>
2025-07-24 18:50:38 +0200

    Create EntityFieldManager

---
+++ PimServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Hynek\Pim;
+
+use Hynek\PackageTools\Package;
+use Hynek\PackageTools\PackageToolsServiceProvider;
+
+class PimServiceProvider extends PackageToolsServiceProvider
+{
+
+    public function configurePackage(Package $package): void
+    {
+        //
+    }
+}
